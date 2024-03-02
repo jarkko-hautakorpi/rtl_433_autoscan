b3ff078b9c391f61701b01ae2e84a0d7b07236e6
Jarkko Hautakorpi <jarkko.hautakorpi@gmail.com>
2024-03-02 13:48:07 +0200

    Try scanning range of frequencies.

--- start_autoscan.py
+++ start_autoscan.py
@@ -1,14 +1,15 @@
 import subprocess
 import time
-import itertools
 import signal
 import sys
 
 # Frequencies (in Hz) to scan: 433.92 MHz, 868 MHz, 915 MHz
-frequencies = [433920000, 868000000, 915000000]
+# frequencies = [433920000, 868000000, 915000000]
 
-# Demodulator protocols to cycle through (example: enabling Acurite and enabling Oregon Scientific)
-protocols = ['-R 40', '-R 41']
+
+# Generate a list of frequencies in 1MHz spacing to scan
+def generate_frequencies(start=345000000, end=440000000, step=1000000):
+    return list(range(start, end + step, step))
 
 
 # Handle Ctrl-C gracefully
@@ -20,9 +21,9 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 
-def run_rtl_433(freq, protocol):
-    # Construct the rtl_433 command
-    cmd = ['rtl_433', '-f', str(freq), protocol, '-T', '300']
+def run_rtl_433(freq, interval=30):
+    # Construct the rtl_433 command with -G to enable all protocols
+    cmd = ['rtl_433', '-f', str(freq), '-G', '4', '-S', 'all', '-T', '30']
     print(f"Running {' '.join(cmd)}")
     try:
         # Run the command and wait for it to complete
@@ -32,8 +33,14 @@ def run_rtl_433(freq, protocol):
 
 
 def main():
-    for freq, protocol in itertools.product(frequencies, protocols):
-        run_rtl_433(freq, protocol)
+    frequencies = generate_frequencies()
+    interval = 30  # seconds
+    # calculate seconds to run the command and convert to minutes
+    totaltime = interval * frequencies.__len__() / 60
+    print(f"List has: {frequencies.__len__()} number of frequencies to scan. {interval} seconds each.")
+    print(f"Total time to run: {totaltime} minutes list of qty of frequencies: {frequencies.__len__()}")
+    for freq in frequencies:
+        run_rtl_433(freq)
 
 
 if __name__ == '__main__':
