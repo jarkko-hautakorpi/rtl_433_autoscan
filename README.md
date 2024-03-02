502ac0bea33a6abdd80e81274224a12b53fe310f
Jarkko Hautakorpi <jarkko.hautakorpi@gmail.com>
2024-03-02 15:39:08 +0200

    example: python3 start_autoscan.py 30 345000000 440000000 1000000

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ This is a collection of scripts to automate the scanning with RTL_433
 
 see: [merbanan/rtl_433](https://github.com/merbanan/rtl_433)
 
+### Usage
+start_autoscan.py seconds start_freq end_freq step_freq
+```bash
+python3 start_autoscan.py 30 345000000 440000000 1000000
+```
--- start_autoscan.py
+++ start_autoscan.py
@@ -2,10 +2,11 @@ import subprocess
 import time
 import signal
 import sys
+import argparse
+
 
 # Frequencies (in Hz) to scan: 433.92 MHz, 868 MHz, 915 MHz
 # frequencies = [433920000, 868000000, 915000000]
-
 
 # Generate a list of frequencies in 1MHz spacing to scan
 def generate_frequencies(start=345000000, end=440000000, step=1000000):
@@ -23,7 +24,7 @@ signal.signal(signal.SIGINT, signal_handler)
 
 def run_rtl_433(freq, interval=30):
     # Construct the rtl_433 command with -G to enable all protocols
-    cmd = ['rtl_433', '-f', str(freq), '-G', '4', '-S', 'all', '-T', '30']
+    cmd = ['rtl_433', '-f', str(freq), '-G', '4', '-S', 'all', '-T', str(interval)]
     print(f"Running {' '.join(cmd)}")
     try:
         # Run the command and wait for it to complete
@@ -33,16 +34,35 @@ def run_rtl_433(freq, interval=30):
 
 
 def main():
-    frequencies = generate_frequencies()
-    interval = 30  # seconds
+    # Create the parser
+    parser = argparse.ArgumentParser(description="example: python3 start_autoscan.py 30 345000000 440000000 1000000")
+
+    # Add arguments
+    parser.add_argument('--sec', type=int, default=30,
+                        help='Time in seconds (default: 30)')
+    parser.add_argument('--start', type=int, default=345000000,
+                        help='Starting frequency (default: 345000000)')
+    parser.add_argument('--stop', type=int, default=440000000,
+                        help='End frequency (default: 440000000)')
+    parser.add_argument('--step', type=int, default=1000000,
+                        help='Frequency step (default: 1000000)')
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # If the first command line parameter is given, use it as a number and use it as the interval of seconds
+    interval = args.sec
+    frequencies = generate_frequencies(args.start, args.stop, args.step)
     # calculate seconds to run the command and convert to minutes
     totaltime = interval * frequencies.__len__() / 60
-    print(f"List has: {frequencies.__len__()} number of frequencies to scan. {interval} seconds each.")
-    print(f"Total time to run: {totaltime} minutes list of qty of frequencies: {frequencies.__len__()}")
-    for freq in frequencies:
-        run_rtl_433(freq)
+    print(f"*")
+    print(f"* List has: {frequencies.__len__()} frequencies to scan. {interval} seconds each.")
+    print(f"* Total time to run: {totaltime} minutes for list of {frequencies.__len__()} frequencies.")
+    print(f"*")
+    while True:
+        for freq in frequencies:
+            run_rtl_433(freq, interval)
 
 
 if __name__ == '__main__':
-    while True:
-        main()
+    main()
